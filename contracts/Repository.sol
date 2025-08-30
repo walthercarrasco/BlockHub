f0db29338ebc7026fe955e9a9992adbb94a57144
AlexVila0204 <alberth@unitec.edu>
2025-08-30 16:16:40 -0600

    Implementación de contratos para gestión de badges y repositorios, incluyendo la creación de BlockhubPOAP, mejoras en Repository y RepositoryFactory, y lógica para asignación de badges por commits y repositorios creados.

--- contracts/Repository.sol
+++ contracts/Repository.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity ^0.8.28;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -9,73 +9,95 @@ contract Repository is ERC721{
         string commitMsg;
         uint256 timestamp;
         address committer;
+        string commitCID;
+        uint256 status;
     }
     string repoName;
     string repoFolderCID;
     address repoOwnerAddr;
-    Commit[] private _commits; // array to store commit CIDs
+    Commit[] private _commits;
 
     constructor(string memory _repoName, string memory _repoCID, address _creator) ERC721("RepositoryNFT", "REPO"){
         repoName = _repoName;
         repoFolderCID = _repoCID;
         repoOwnerAddr = _creator;
-
+        _safeMint(_creator, 1); 
     }
+
+    receive() external payable {}
 
     function getRepoFolderCID() external view returns (string memory) {
         return repoFolderCID;
     }
 
+    function getRepoName() external view returns (string memory) {
+        return repoName;
+    }
 
-   //Modificador para indicar que solo el dueno puede ejecutar
-    modifier onlyRepoOwner() {
-        require(ownerOf(1) == msg.sender, "No eres el dueno del repositorio");
+    modifier onlyRepoOwner(address sender) {
+        require(sender == repoOwnerAddr, "No eres el dueno del repositorio");
         _;
     }
 
-    //  Simulacion de "aprobar un cambio importante"
-    function approveChange(string calldata changeHash, uint256 rewardAmount) external onlyRepoOwner {
-        emit ChangeApproved(changeHash, msg.sender, repoOwnerAddr, rewardAmount);
-    }
 
-    event ChangeApproved(
-        string changeHash,
-        address contributor,
-        address approvedBy,
-        uint256 rewardAmount
-    );
-
-    // Add a new commit
-    function addCommit(string memory _commitMsg, address _committer) external {
+    function addPendingCommit(string memory _commitMsg, address _committer, string memory _commitCID) external {
         Commit memory newCommit = Commit({
             commitMsg: _commitMsg,
-            timestamp: block.timestamp,  // current block timestamp
-            committer: _committer
+            timestamp: block.timestamp, 
+            committer: _committer,
+            commitCID: _commitCID,
+            status: 0
         });
         _commits.push(newCommit);
     }
 
-  function getAllCommits() external view returns (string[] memory messages, uint256[] memory timestamps, address[] memory committers) {
+    function getCommits() external view returns (
+        string[] memory messages, 
+        uint256[] memory timestamps, 
+        address[] memory committers, 
+        uint256 [] memory status) 
+        {
         uint256 count = _commits.length;
         messages = new string[](count);
         timestamps = new uint256[](count);
         committers = new address[](count);
+        status = new uint256[](count);
 
         for (uint256 i = 0; i < count; i++) {
             Commit storage c = _commits[i];
             messages[i] = c.commitMsg;
             timestamps[i] = c.timestamp;
             committers[i] = c.committer;
+            status[i] = c.status;
         }
+    } 
+
+    function acceptCommit(uint256 _commitIndex, uint256 rewardAmount, address sender) external payable onlyRepoOwner(sender){
+        Commit storage c = _commits[_commitIndex];
+        require(c.status == 0, "Commit already processed");
+        require(address(this).balance >= rewardAmount, "Not enough ETH in contract");
+
+        // Mark commit as accepted
+        c.status = 1;
+        repoFolderCID = c.commitCID;
+
+        // Pay committer
+        (bool success, ) = c.committer.call{value: rewardAmount}("");
+        require(success, "Payment failed");
     }
 
-    // Get owner
+    function rejectCommit(uint256 _commitIndex, address sender) external onlyRepoOwner(sender) returns (address){
+        Commit storage c = _commits[_commitIndex];
+        require(c.status == 0, "Commit already processed");
+        c.status = 2;
+        return c.committer;
+    }
+
+    function getBalance() external view returns (uint256) {
+        return address(this).balance;
+    }
+
     function getRepoOwner() external view returns (address) {
         return repoOwnerAddr;
     }
-
-
-
-
-    
 }
--- contracts/RepositoryFactory.sol
+++ contracts/RepositoryFactory.sol
@@ -1,25 +1,58 @@
 // SPDX-License-Identifier: MIT
-
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity ^0.8.28;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "contracts/Repository.sol";
+import "./Repository.sol";
+import "./BlockhubPOAP.sol";
 
 contract RepositoryFactory is ERC721Enumerable {
-    
+
     constructor() ERC721("RepositoryFactory", "REPO") {}
 
     mapping (uint256 => Repository) private repositories;
+    
+    BlockhubPOAP public badgeContract;
+    
+    mapping(address => bool) private hasFirstRepo;
+    mapping(address => bool) private hasFirstCommit;
+    mapping(address => uint256) private userCommitCount;
+    mapping(address => uint256) private userApprovalCount;
+
+    function setBadgeContract(address _badgeContract) external {
+        require(address(badgeContract) == address(0), "Badge contract already set");
+        badgeContract = BlockhubPOAP(_badgeContract);
+    }
 
     function createRepository(string memory _repoName, string memory _repoCID) public {
-        uint256 tokenId = totalSupply() + 1; // token IDs start at 1
+        uint256 tokenId = totalSupply() + 1;
         _safeMint(msg.sender, tokenId);
 
         repositories[tokenId] = new Repository(_repoName, _repoCID, msg.sender);
+
+        if (!hasFirstRepo[msg.sender] && address(badgeContract) != address(0)) {
+            hasFirstRepo[msg.sender] = true;
+            badgeContract.mintBadge(
+                msg.sender,
+                1, 
+                _repoName
+            );
+            badgeContract.updateUserStats(msg.sender, 0, 1, 0); 
+        }
+        
         emit CreatedSuccessfully(tokenId, msg.sender, _repoCID);
     }
+    //Depositar ETH en el repositorio
+    function depositToRepo(uint256 _tokenId) external payable {
+        Repository repo = repositories[_tokenId];
+        require(msg.value > 0, "Must send ETH");
 
-    function getAllCIDsByOwner() external view returns (string[] memory folderCIDs, uint256[] memory tokens) {
+        // Forward ETH to the repository contract
+        (bool success, ) = address(repo).call{value: msg.value}("");
+        require(success, "ETH deposit failed");
+        emit depositedETH(_tokenId, msg.sender, msg.value);
+    }
+
+    function getAllReposByOwner() external view returns (string[] memory folderCIDs, uint256[] memory tokens) {
         uint256 count = balanceOf(msg.sender); // number of NFTs owned
         folderCIDs = new string[](count);
         tokens = new uint256[](count);
@@ -29,33 +62,145 @@ contract RepositoryFactory is ERC721Enumerable {
             tokens[i] = tokenId;
         }
     }
-
-    function processCommit(uint256 _tokenId, string memory message) public {
+    function processNewCommit(
+        uint256 _tokenId, 
+        string memory message, 
+        string memory commitCID) public 
+    {
         Repository repo = repositories[_tokenId];
-        
-        // Add commit to repository
-        repo.addCommit(message, msg.sender);
-
-
-        // Emit event
+        repo.addPendingCommit(message, payable (msg.sender), commitCID);
         emit processedCommit(_tokenId, repo.getRepoOwner(), msg.sender, repo.getRepoFolderCID());
     }
 
-    function retrieveCommits(uint256 _tokenId) public view returns(string[] memory messages, uint256[] memory timestamps, address[] memory committers){
+
+    function processPendingCommit(
+        uint256 _tokenId,
+        string memory message,
+        string memory commitCID
+    ) public {
         Repository repo = repositories[_tokenId];
-        return repo.getAllCommits();
+        repo.addPendingCommit(message, msg.sender, commitCID);
+        
+        if (!hasFirstCommit[msg.sender] && address(badgeContract) != address(0)) {
+            hasFirstCommit[msg.sender] = true;
+            badgeContract.mintBadge(
+                msg.sender,
+                0,
+                repo.getRepoName()
+            );
+        }
+        
+        userCommitCount[msg.sender]++;
+        
+
+        if (userCommitCount[msg.sender] == 5 && address(badgeContract) != address(0)) {
+            badgeContract.mintBadge(
+                msg.sender,
+                2, 
+                "Multiple Repositories"
+            );
+        }
+        
+
+        if (address(badgeContract) != address(0)) {
+            badgeContract.updateUserStats(msg.sender, 1, 0, 0); 
+        }
+        
+        emit processedCommit(_tokenId, repo.getRepoOwner(), msg.sender, repo.getRepoFolderCID());
+    }
+
+    function approveCommit(uint256 _tokenId, uint256 commitIndex, uint256 reward) public payable {
+        Repository repo = repositories[_tokenId];
+        repo.acceptCommit(commitIndex, reward, msg.sender);
+
+        userApprovalCount[msg.sender]++;
+        
+        if (userApprovalCount[msg.sender] == 10 && address(badgeContract) != address(0)) {
+            badgeContract.mintBadge(
+                msg.sender,
+                3, 
+                "Multiple Repositories"
+            );
+        }
+        
+        if (address(badgeContract) != address(0)) {
+            badgeContract.updateUserStats(msg.sender, 0, 0, 1); 
+        }
+        
+        emit approvedCommit(_tokenId, repo.getRepoOwner(), repo.getRepoFolderCID());
+    }
+    
+    function rejectCommit(
+        uint256 _tokenId,
+        uint256 commitIndex) public
+    {
+        Repository repo = repositories[_tokenId];
+        address commiter = repo.rejectCommit(commitIndex, msg.sender);
+        emit rejectedCommit(commiter, repo.getRepoOwner(), repo.getRepoFolderCID());
+    }
+
+    function getUserBadges(address user) external view returns (
+        uint256[] memory,
+        string[] memory,
+        uint256[] memory
+    ) {
+        require(address(badgeContract) != address(0), "Badge contract not set");
+        return badgeContract.getUserBadges(user);
+    }
+    function getAllCIDsByOwner() external view returns (string[] memory folderCIDs, uint256[] memory tokens) {
+        uint256 count = balanceOf(msg.sender);
+        folderCIDs = new string[](count);
+        tokens = new uint256[](count);
+        for (uint256 i = 0; i < count; i++) {
+            uint256 tokenId = tokenOfOwnerByIndex(msg.sender, i);
+            folderCIDs[i] = repositories[tokenId].getRepoFolderCID();
+            tokens[i] = tokenId;
+        }
+    }
+
+    function retrieveCommits(uint256 _tokenId) public view returns(
+        string[] memory messages,
+        uint256[] memory timestamps,
+        address[] memory committers,
+        uint256[] memory status)
+    {
+        Repository repo = repositories[_tokenId];
+        return repo.getCommits();
+    }
+
+    function getBalance(uint256 _tokenId) public view returns(uint256){
+        return repositories[_tokenId].getBalance();
     }
 
     event CreatedSuccessfully(
-        uint256 indexed tokenId,       // ID of the NFT minted
-        address indexed owner,        // Name of the repository
-        string repoCID               // IPFS folder CID
+        uint256 indexed tokenId,
+        address indexed owner,
+        string repoCID
     );
 
     event processedCommit(
-        uint256 indexed tokenId,       // ID of the NFT minted
-        address indexed owner,        // Name of the repository
-        address indexed committer,    // Address of the committer
-        string repoCID               // IPFS folder CID
+        uint256 indexed tokenId,
+        address indexed owner,
+        address indexed committer,
+        string repoCID
     );
+
+    event approvedCommit(
+        uint256 indexed tokenId,
+        address indexed owner,
+        string repoCID
+    );
+
+    event rejectedCommit(
+        address indexed committer,
+        address indexed rejectedBy,
+        string repoCID
+    );
+
+    event depositedETH(
+        uint256 indexed tokenId,      
+        address indexed owner,        
+        uint256 amount
+    );
+
 }
